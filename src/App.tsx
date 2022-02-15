00f0a6141e68ca84d9e5db176d25f7c538f6cbb1
Grosno <pbc20092yandex.ru>
2022-02-15 10:21:28 +0300

    V-01 Создан проект react+ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Hello</h1>
+    </header>
+  </div>
+);
+
+export default App;
