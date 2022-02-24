d20c595780f455057294b43336363b09ab24aeef
grosno <pbc2009@yandex.ru>
2022-02-24 18:24:02 +0300

    V-02 Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-// import 'antd/dist/antd.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux/store';
