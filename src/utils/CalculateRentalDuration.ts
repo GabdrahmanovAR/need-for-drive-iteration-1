c24f2295f88b3a06bd1285dbc0e4398973a0fe68
grosno <pbc2009@yandex.ru>
2022-03-20 17:37:00 +0300

    F-06 Скопирован проект с F-05

--- src/utils/CalculateRentalDuration.ts
+++ src/utils/CalculateRentalDuration.ts
@@ -7,13 +7,13 @@ export const CalculateRentalDuration = (dateFrom: string, dateTo: string) => {
   const duration = moment.duration(startDate.diff(endDate));
   if (Math.abs(duration.asDays()) < 1) {
     const hours = Math.floor(Math.abs(duration.asHours()));
-    const minutes = Math.floor((Math.abs(duration.asMinutes()) % 1) * 60);
+    const minutes = Math.floor(((Math.abs(duration.asMinutes()) / 60) % 1) * 60);
     return `${hours}ч ${minutes}м`;
   }
   if (Math.abs(duration.asDays()) > 1) {
     const days = Math.floor(Math.abs(duration.asDays()));
-    const hours = Math.floor(Math.abs(24 + duration.asHours()));
-    const minutes = Math.floor((Math.abs(duration.asMinutes()) % 1) * 60);
+    const hours = Math.floor(Math.floor(Math.abs(duration.asHours())) - (24 * days));
+    const minutes = Math.floor(((Math.abs(duration.asMinutes()) / 60) % 1) * 60);
     return `${days}д ${hours}ч ${minutes}м`;
   }
   return 'Не выбрано';
